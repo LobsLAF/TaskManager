d0dd18ae89fb26075850bcf78869b2e1e9401814
LobsLAF <linombm2099@gmail.com>
2024-01-26 15:17:40 -0300

    Estrelas de dificuldade adicionadas

--- lib/main.dart
+++ lib/main.dart
@@ -13,38 +13,52 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
         useMaterial3: true,
       ),
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.amber,
-          title: Text('ToDo list!'),
+          leading: Container(),
+          title: Text(
+            'Tarefas:',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
-        body: ListView(
-          children: [
-            Task('Aprender Flutter'),
-            Task('Meditar'),
-            Task('Andar de Bike'),
-          ],
+        body: Container(
+          color: Colors.amber[100],
+          child: ListView(
+            children: [
+              Task(5, 'Aprender Flutter',
+                  'https://pbs.twimg.com/media/FKNlhKZUcAEd7FY?format=jpg&name=4096x4096'),
+              Task(0, 'Meditar',
+                  'https://4.bp.blogspot.com/-9UBNrgGg-tk/VhrDzvW7DdI/AAAAAAAATT8/-QDGNJPh_fw/s1600/2.jpg'),
+              Task(2, 'Andar de Bike',
+                  'https://cdn.brujulabike.com/uploads/images/emtb_bicicleta_electrina_montana.jpg'),
+            ],
+          ),
         ),
-        floatingActionButton: FloatingActionButton(onPressed: () {}),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.amber,
+        ),
       ),
     );
   }
 }
 
 class Task extends StatefulWidget {
-  const Task(this.nome, {super.key});
+  const Task(this.dificuldade, this.nome, this.imagem, {super.key});
 
   final String nome;
+  final String imagem;
+  final int dificuldade;
 
   @override
   State<Task> createState() => _TaskState();
 }
 
 class _TaskState extends State<Task> {
-
   int nivel = 0;
 
   @override
@@ -61,26 +75,80 @@ class _TaskState extends State<Task> {
             Column(
               children: [
                 Container(
-                  color: Colors.amber[100],
+                  color: Colors.white,
                   height: 100,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Container(
                         color: Colors.grey,
-                        width: 72,
-                      ),
-                      Container(
-                        width: 200,
-                        child: Text(
-                          widget.nome,
-                          style: TextStyle(
-                              fontSize: 24, overflow: TextOverflow.ellipsis),
+                        width: 100,
+                        height: 100,
+                        child: Image.network(
+                          widget.imagem,
+                          fit: BoxFit.cover,
                         ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 190,
+                            child: Text(
+                              textAlign: TextAlign.center,
+                              widget.nome,
+                              style: TextStyle(
+                                  fontSize: 22,
+                                  overflow: TextOverflow.ellipsis),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 1)
+                                    ? Colors.amber
+                                    : Colors.amber[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 2)
+                                    ? Colors.amber
+                                    : Colors.amber[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 3)
+                                    ? Colors.amber
+                                    : Colors.amber[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 4)
+                                    ? Colors.amber
+                                    : Colors.amber[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 5)
+                                    ? Colors.amber
+                                    : Colors.amber[100],
+                              ),
+                            ],
+                          )
+                        ],
                       ),
                       Padding(
                         padding: const EdgeInsets.all(10.0),
                         child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.amber,
+                            ),
                             onPressed: () {
                               setState(() {
                                 nivel++;
@@ -90,11 +158,16 @@ class _TaskState extends State<Task> {
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                Icon(Icons.arrow_upward_sharp),
-                                Text('Lvl up')
+                                Icon(
+                                  Icons.arrow_upward_sharp,
+                                  color: Colors.white,
+                                ),
+                                Text(
+                                  'Lvl up',
+                                  style: TextStyle(color: Colors.white),
+                                )
                               ],
-                            )
-                          ),
+                            )),
                       )
                     ],
                   ),
@@ -109,7 +182,7 @@ class _TaskState extends State<Task> {
                         child: LinearProgressIndicator(
                           backgroundColor: Colors.amber[300],
                           color: Colors.white,
-                          value: nivel/15,
+                          value: (widget.dificuldade > 0) ? nivel / 10 / widget.dificuldade : 1,
                         ),
                       ),
                     ),
